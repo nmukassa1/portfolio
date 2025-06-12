7842506f8d5e3ba13aefca021df5743b8f785640
nmukassa1 <nmukassa1@gmail.com>
2025-06-12 20:35:01 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,14 @@ import Header from "./components/Header";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Home() {
+  useEffect(() => {
+    let smoother = ScrollSmoother.create({
+      wrapper: "#scroll-wrapper",
+      content: "#scroll-content",
+      smooth: 1.5,
+      smoothTouch: 0.1,
+    });
+  }, []);
   return (
     <div id="scroll-wrapper" className="px-4 pb-14">
       <div id="scroll-content" className="relative">
